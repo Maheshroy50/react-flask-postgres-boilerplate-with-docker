ccaf36d66e40b62edaff490d9a36ef76b2780a8d
Mahesh Rao <maheshrao@Maheshs-MacBook-Air.local>
2025-11-22 19:54:16 +0530

    added fix to lint

--- backend/api/config.py
+++ backend/api/config.py
@@ -1,5 +1,7 @@
 class Config(object):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://myuser:mypassword@192.168.99.100/sport_stats'
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql+psycopg2://myuser:mypassword@192.168.99.100/sport_stats"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
